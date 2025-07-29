b82a1c9d580e0c036de4be35f5398b243fb49c1e
Toluwani 🔗 <israelmakanjuola7@gmail.com>
2025-07-29 14:40:26 +0100

    Student view course page setup

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -92,7 +92,7 @@ class OrderController extends Controller
         $id = Auth::user()->id;
 
         $course = Order::where('course_id', $course_id)->where('user_id', $id)->first();
-        $section = CourseSection::where('couse_id', $course_id)->orderBy('id','asc')->get();
+        $section = CourseSection::where('course_id', $course_id)->orderBy('id','asc')->get();
 
         return view('frontend.mycourse.course_view', compact('course', 'section'));
     }
