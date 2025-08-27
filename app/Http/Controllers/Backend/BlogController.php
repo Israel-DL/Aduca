a65776a47fb1843530b81772aafc0b48cb1b7118
Toluwani 🔗 <israelmakanjuola7@gmail.com>
2025-08-27 13:51:16 +0100

    Blog Pagination

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -196,7 +196,7 @@ class BlogController extends Controller
 
     public function Blog(){
 
-        $blog = BlogPost::latest()->get();
+        $blog = BlogPost::latest()->paginate(2);
         $bcategory = BlogCategory::latest()->get();
         $post = BlogPost::latest()->limit(3)->get();
         return view('frontend.blog.blog_list', compact('blog', 'post','bcategory'));
